b8a85aa43d57f42c16700cd6d81003e894bd619f
Sevriukoff <igor.sevriukoff@yandex.ru>
2023-10-14 14:51:22 +0700

    Added logo and buttons to the header

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,11 +1,18 @@
 import React, {FC} from 'react';
 import styles from './Header.module.scss'
-
+import logoImg from '../../resources/logo.svg';
+import downloadImg from '../../resources/download.svg'
+import steamImg from '../../resources/steam.svg'
+import Button from "../../components/Button/Button";
 
 const Header : FC = () => {
     return (
-        <header>
-            <button className={styles.active}/>
+        <header className={styles.header}>
+            <img src={logoImg} alt={'logo'}/>
+            <div>
+                <Button className={styles.header__btn} icon={downloadImg} >Download</Button>
+                <Button icon={steamImg} isOutlined={true}>Login</Button>
+            </div>
         </header>
     );
 };
